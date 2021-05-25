d63635daf9ec561b75fc41fecc43fa2467432d0d
SabitTheBot <iamsabit99@gmail.com>
2021-05-26 02:21:18 +0600

    logout done

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,34 +1,58 @@
-import React from 'react'
-import {Link, withRouter} from  'react-router-dom'
-import {signout} from '../auth/index'
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import { signout } from "../auth/index";
 
+const isActive = (history, path) => {
+  if (history.location.pathname === path) {
+    return { color: "red" };
+  } else {
+    return { color: "black" };
+  }
+};
 
-const isActive = (history,path) =>{
-    if(history.location.pathname === path) {
-        return {color: "red"}
-    }
-    else{
-        return {color: "black"}
-    }
-}
+const Menu = ({ history }) => {
+  return (
+    <div>
+      <ul className="nav nav-tabs bg-primary">
+        <li className="nav-item">
+          <Link className="nav-link" style={isActive(history, "/")} to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/signin")}
+            to="/signin"
+          >
+            Signin
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            className="nav-link"
+            style={isActive(history, "/signup")}
+            to="/signup"
+          >
+            Signup
+          </Link>
+        </li>
+        <li className="nav-item">
+          <span
+            className="nav-link"
+            style={{ cursor: "pointer", color: "black" }}
+            onClick={() =>
+              signout(() => {
+                history.push("/");
+              })
+            }
+          >
+            Signout
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-const Menu = ( {history}) => {
-    return (
-        <div>
-            <ul className="nav nav-tabs bg-primary">
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, '/signin')}  to="/signin">Signin</Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
-                </li>
-
-            </ul>
-        </div>
-    )
-}
-
-export default  withRouter(Menu);
+export default withRouter(Menu);
