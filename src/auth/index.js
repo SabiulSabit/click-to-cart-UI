c57886b5d2e49e98d3f91d3313334ab65d5ab5d9
SabitTheBot <iamsabit99@gmail.com>
2021-05-26 02:27:35 +0600

    add isAuthenticate for authetication cheeck

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,70 +1,76 @@
 import { API } from "../config";
 
-
 //user signup
-  export const signup = (user) => {
-    // console.log(name, email, password)
-    return fetch(`${API}/signup`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
+export const signup = (user) => {
+  // console.log(name, email, password)
+  return fetch(`${API}/signup`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((res) => {
+      return res.json();
     })
-      .then((res) => {
-        return res.json();
-      })
-      .catch((err) => {
-        return err;
-      });
-  };
-
-
+    .catch((err) => {
+      return err;
+    });
+};
 
 //user signin
-  export const signin = (user) => {
-    // console.log(name, email, password)
-    return fetch(`${API}/signin`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(user),
+export const signin = (user) => {
+  // console.log(name, email, password)
+  return fetch(`${API}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((res) => {
+      return res.json();
     })
-      .then((res) => {
-        return res.json();
-      })
-      .catch((err) => {
-        return err;
-      });
-  };
+    .catch((err) => {
+      return err;
+    });
+};
 
-
-  //save jwt to local storage
-export const authenticate =  (data, next) =>{
-       
-  if(typeof window !== undefined){
-    localStorage.setItem('jwt', JSON.stringify(data));
+//save jwt to local storage
+export const authenticate = (data, next) => {
+  if (typeof window !== undefined) {
+    localStorage.setItem("jwt", JSON.stringify(data));
     next();
   }
-}
-
+};
 
 //signout
-export const signout = (next) =>{
-  if(typeof window !== undefined){
-    localStorage.removeItem('jwt');
+export const signout = (next) => {
+  if (typeof window !== undefined) {
+    localStorage.removeItem("jwt");
     next();
-    return fetch( `${API}/signout`, {
-      method: "GET"
+    return fetch(`${API}/signout`, {
+      method: "GET",
     })
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err);
-    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-}
+};
+
+//is authenticte
+export const isAuthenticate = () => {
+  if (typeof window == undefined) {
+    return false;
+  }
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
