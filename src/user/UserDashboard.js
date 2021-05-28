979e4970b10ded4fb3f448f391a1e37b27fe175a
SabitTheBot <iamsabit99@gmail.com>
2021-05-28 21:14:05 +0600

    add basic component cide

---
+++ src/user/UserDashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from  '../core/Layout'
+
+
+const Dashboard = () => {
+    return (
+        <Layout title="User Dashboard" description="User Dashboard">
+            123
+        </Layout>
+    )
+}
+
+export default Dashboard;
