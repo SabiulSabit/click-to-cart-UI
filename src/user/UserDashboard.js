5656cceaf7e7737922a83f5af8dc68318fc1ed84
SabitTheBot <iamsabit99@gmail.com>
2021-05-29 17:25:24 +0600

    add basic deisng in dashboard

--- src/user/UserDashboard.js
+++ src/user/UserDashboard.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import Layout from  '../core/Layout'
+import {Container}  from 'react-bootstrap'
+import {isAuthenticate} from '../auth/index'
 
 
 const Dashboard = () => {
     return (
         <Layout title="User Dashboard" description="User Dashboard">
-            123
+            <Container>
+            <div className="card mb-5">
+                <h3 className="card-header">User Information</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">
+                            Name
+                    </li>
+                    <li className="list-group-item">
+                            Email
+                    </li>
+                    <li className="list-group-item">
+                            Role
+                    </li>
+                </ul>
+            </div>
+            <div className="card">
+                 <h3 className="card-header">Purchase History</h3>
+                 <ul className="list-group">
+                    <li className="list-group-item">
+                            History
+                    </li>
+                </ul>
+            </div>
+            </Container>
         </Layout>
     )
 }
