85574b7931ca479c4320a0a8f972b97a728fdd65
SabitTheBot <iamsabit99@gmail.com>
2021-05-29 18:44:38 +0600

    admin dashboard basic code done

---
+++ src/user/AdminDashboard.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Layout from "../core/Layout";
+import { Container, Row, Col } from "react-bootstrap";
+import { isAuthenticate } from "../auth/index";
+import { Link } from "react-router-dom";
+
+const AdminDashboard = () => {
+  const {
+    user: { _id, name, email, role },
+  } = isAuthenticate();
+
+  const adminLinks = () => {
+    return (
+      <div className="card">
+        <h4 className="card-header">Admin Links</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link className="nav-link" to="/create/category">
+              Create Category
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link className="nav-link" to="/create/product">
+              Create Product
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const adminInfo = () => {
+    return (
+      <div className="card mb-5">
+        <h3 className="card-header">Admin Information</h3>
+        <ul className="list-group">
+          <li className="list-group-item">{name}</li>
+          <li className="list-group-item">{email}</li>
+          <li className="list-group-item">
+            {role === 1 ? "Admin" : "Registred User"}
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  return (
+    <Layout title="User Dashboard" description="User Dashboard">
+      <Container>
+            <Row>
+                <Col md={3}>
+                    {adminLinks()}
+                </Col>
+                <Col md={9}>
+                    {adminInfo()}
+                </Col>
+            </Row>
+      </Container>
+    </Layout>
+  );
+};
+
+export default AdminDashboard;
