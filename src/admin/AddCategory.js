dfee797822de12c41724e7e20f4c47238a379e7f
SabitTheBot <iamsabit99@gmail.com>
2021-05-31 16:54:13 +0600

    category create done

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -1,71 +1,93 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import  {Link} from "react-router-dom"
 import Layout from "../core/Layout";
 import { Container, Row, Col } from "react-bootstrap";
 import { isAuthenticate } from "../auth/index";
-import { Link } from "react-router-dom";
+import { createCategory } from "./apiAdmin";
 
+const AddCategory = () => {
+  let [error, setError] = useState(0);
+  let [success, setSuccess] = useState(0);
 
-const AddCategory = () =>{
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
-    let [error, setError] = useState(0);
-    let [success, setSuccess] = useState(0);
+  //token
+  const { user, token } = isAuthenticate();
 
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        formState: { errors },
-      } = useForm();
-    
-    //token
-    const {user, token} = isAuthenticate();   
+  let onSubmit = (data) => {
+    setSuccess(0);
+    createCategory(user._id, token, data).then((data) => {
+      if (data.error) {
+        setSuccess(0);
+        setError(1);
+      } else {
+        setSuccess(1);
+        setValue("name", "", { shouldValidate: false });
+        setError(0);
+      }
+    });
+  };
 
+  const newCategoryForm = () => (
+    <Container className="center">
+      <Row>
+        <Col md={8} className="offset-md-2">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label htmlFor="name" className="text-muted">
+                Name{" "}
+                <span className="err">
+                  {errors.name && "This Fields is Required"}
+                </span>
+              </label>
+              <input
+                type="text"
+                id="name"
+                placeholder="Your Name"
+                className="form-control"
+                {...register("name", { required: true, maxLength: 32 })}
+              />
+            </div>
 
-    let onSubmit = (data) => {
-            setSuccess(0);
-            console.log(data);
-
-    };
-
-    const newCategoryForm = () => ( 
-        <Container className="center">
-        <Row>
-          <Col md={8} className="offset-md-2">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="form-group">
-                <label htmlFor="name" className="text-muted">
-                  Name{" "}
-                  <span className="err">
-                    {errors.name && "This Fields is Required"}
-                  </span>
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  placeholder="Your Name"
-                  className="form-control"
-                  {...register("name", { required: true, maxLength: 32 })}
-                />
-              </div>
-          
-              <button className="btn btn-outline-primary" type="submit">
-                Create Category
-              </button>
-            </form>
-          </Col>
-        </Row>
-      </Container>
-    )
-  
-
-   
-  return (
-    <Layout title="Add New Category" description="E-Commerce Website">
-        {newCategoryForm()}
-    </Layout>
+            <button className="btn btn-outline-primary" type="submit">
+              Create Category
+            </button>
+          </form>
+          <div className="mt-5">
+              <Link to='/admin/dashboard' className="text-warning">Back to Dashboard</Link>
+      </div>
+        </Col>
+      </Row>
+    </Container>
   );
 
-}
+  const showSuccess = () => {
+    if (success) {
+      return <h3 className="text-success text-center"> Category Create Successfuly!! </h3>;
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <h3 className="text-danger text-center"> Category Should be Unique </h3>;
+    }
+  };
+
+
+
+  return (
+    <Layout title="Add New Category" description="E-Commerce Website">
+      {showError()}
+      {showSuccess()}
+      {newCategoryForm()}
+    </Layout>
+  );
+};
 
 export default AddCategory;
--- src/auth/AdminRoute.js
+++ src/auth/AdminRoute.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { isAuthenticate } from "./index";
-import AdminDashboard from "../user/AdminDashboard";
+
 
 function AdminRoute({ component ,path, children, ...rest }) {
   if (isAuthenticate() && isAuthenticate().user.role === 1) {
