b939f11b62bb9ce14bbb1c194647b53df6eddc45
SabitTheBot <iamsabit99@gmail.com>
2021-06-13 21:42:35 +0600

    fetch added for get all categories

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -1,37 +1,31 @@
 import { API } from "../config";
 
-
 //create category
 export const createCategory = (userId, token, category) => {
-      
-    return fetch(`${API}/category/create/${userId}`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
-      },
-      body: JSON.stringify(category),
+  return fetch(`${API}/category/create/${userId}`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(category),
+  })
+    .then((res) => {
+      return res.json();
     })
-      .then((res) => {
-        return res.json();
-      })
-      .catch((err) => {
-        return err;
-      });
-  };
+    .catch((err) => {
+      return err;
+    });
+};
 
-
-
-  
 //create product
 export const createProduct = (userId, token, product) => {
-
   return fetch(`${API}/product/create/${userId}`, {
     method: "POST",
     headers: {
       Accept: "application/json",
-      Authorization: `Bearer ${token}`
+      Authorization: `Bearer ${token}`,
     },
     body: product,
   })
@@ -45,5 +39,13 @@ export const createProduct = (userId, token, product) => {
 
 //get all category
 export const getCategorys = () => {
-  
-}  
+  return fetch(`${API}/categories`, {
+    method: "GET",
+  })
+    .then((data) => {
+      return data.json();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
