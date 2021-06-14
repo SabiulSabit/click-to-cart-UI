b273c1e989c568106621cc7bee61128ed6169bfa
SabitTheBot <iamsabit99@gmail.com>
2021-06-14 13:57:11 +0600

    products by sell method added

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Layout from './Layout'
+import {getProducts} from './apiCore'
 
 const Home = () => {
+    
+    const [productsBySell, setProductsBySell] = useState([]);
+    const [productsByArival, setProductsByArival] = useState([]);
+    const [error, setError] = useState(false);
+   
+    //get the products by sell
+    const loadProductsBySell = ( ) => {
+            
+        getProducts("sold").then(data => {
+            if(data.error){
+                setError(data.error);
+            }
+            else{
+                setProductsBySell(data);
+            }
+        })
+    }
+
     return (
         <Layout title="Home" description="E-Commerce Website">
             ...
