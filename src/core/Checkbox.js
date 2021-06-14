0657f1b6bb4667e1a8a05a50ded5692a1f1f207b
SabitTheBot <iamsabit99@gmail.com>
2021-06-14 18:18:45 +0600

    checked-uncheckd check done

--- src/core/Checkbox.js
+++ src/core/Checkbox.js
@@ -1,9 +1,31 @@
 import React, { useState, useEffect } from "react";
 
 const Checkbox = ({ categories }) => {
+
+  const [checked, setChecked] = useState([]);
+ 
+  const handelToggle = c => () => {
+    
+    const currentCategoryId = checked.indexOf(c);
+    const newCheckedCategoryId = [...checked];
+   
+    //check not  found
+    if(currentCategoryId === -1) {
+         newCheckedCategoryId.push(c);
+    }
+    else{
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
+
+    console.log(newCheckedCategoryId);
+    setChecked(newCheckedCategoryId);
+
+
+  }
+
   return categories.map((c, i) => (
     <li key={i} className="list-unstyled">
-      <input type="checkbox" className="form-check-input" />
+      <input onChange={handelToggle(c._id)} value={checked.indexOf(c._id === -1 )} type="checkbox" className="form-check-input" />
       <label className="form-check-label">{c.name}</label>
     </li>
   ));
