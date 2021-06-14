b3b1aca41fc13abea4d365249f7e41be2ae55ff2
SabitTheBot <iamsabit99@gmail.com>
2021-06-14 14:06:33 +0600

    add Card.js Component

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,44 +1,47 @@
-import React, {useState, useEffect} from 'react'
-import Layout from './Layout'
-import {getProducts} from './apiCore'
+import React, { useState, useEffect } from "react";
+import Layout from "./Layout";
+import { getProducts } from "./apiCore";
 
 const Home = () => {
-    
-    const [productsBySell, setProductsBySell] = useState([]);
-    const [productsByArrival, setProductsByArrival] = useState([]);
-    const [error, setError] = useState(false);
-   
-    //get the products by sell
-    const loadProductsBySell = ( ) => {
-            
-        getProducts("sold").then(data => {
-            if(data.error){
-                setError(data.error);
-            }
-            else{
-                setProductsBySell(data);
-            }
-        })
-    }
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
 
-        //get the products by arrival
-        const loadProductsByArrival = ( ) => {
-            
-            getProducts("createdAt").then(data => {
-                if(data.error){
-                    setError(data.error);
-                }
-                else{
-                    setProductsBySell(data);
-                }
-            })
-        }
+  //get the products by sell
+  const loadProductsBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
 
-    return (
-        <Layout title="Home" description="E-Commerce Website">
-            ...
-        </Layout>
-    )
-}
+  //get the products by arrival
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
 
-export default Home; 
+
+  useEffect( () => {
+        loadProductsByArrival();
+        loadProductsBySell();
+  }, [] )
+
+  return (
+    <Layout title="Home" description="E-Commerce Website">
+      {JSON.stringify(productsByArrival)}
+      <hr />
+      {JSON.stringify(productsBySell)}
+    </Layout>
+  );
+};
+
+export default Home;
