150d24d2b25cfcf95f966509300afe9f3e06b41a
SabitTheBot <iamsabit99@gmail.com>
2021-06-16 14:46:55 +0600

    all category data fetch done

--- src/core/Search.js
+++ src/core/Search.js
@@ -1,15 +1,40 @@
 import React, { useState, useEffect } from "react";
-import { getProducts } from "./apiCore";
+import { getCategorys } from "./apiCore";
 import { Container, Row, Col } from "react-bootstrap";
 import Card from "./Card";
 
 const Search = () => {
-    // const [data, setData] = useState({
-    //     categ
-    // })
+    const [data, setData] = useState({
+        categories: [],
+        category: "",
+        search: '',
+        result: [],
+        searched: false,
+    })
+
+    const  {categories, category, search, result, searched} = data;
+
+    //get all categories
+    const loadCategory = () => {
+        getCategorys().then(data => {
+            if(data.error){
+                console.log(data.error)
+            }
+            else{
+                setData({...data, categories: data})
+            }
+        })
+    }
+
+    useEffect( () => {
+        loadCategory();
+    }, [])
+
     return (
         <div>
-            <h2>Search Bar</h2>
+           <div>
+               {}
+           </div>
         </div>
     )
 }
