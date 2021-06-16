0c2e0a7dbe43ea89e3c752b32ebba81e15eb2730
SabitTheBot <iamsabit99@gmail.com>
2021-06-16 15:01:25 +0600

    search bar show done

--- src/core/Search.js
+++ src/core/Search.js
@@ -4,39 +4,70 @@ import { Container, Row, Col } from "react-bootstrap";
 import Card from "./Card";
 
 const Search = () => {
-    const [data, setData] = useState({
-        categories: [],
-        category: "",
-        search: '',
-        result: [],
-        searched: false,
-    })
+  const [data, setData] = useState({
+    categories: [],
+    category: "",
+    search: "",
+    result: [],
+    searched: false,
+  });
 
-    const  {categories, category, search, result, searched} = data;
+  const { categories, category, search, result, searched } = data;
 
-    //get all categories
-    const loadCategory = () => {
-        getCategorys().then(data => {
-            if(data.error){
-                console.log(data.error)
-            }
-            else{
-                setData({...data, categories: data})
-            }
-        })
-    }
+  //get all categories
+  const loadCategory = () => {
+    getCategorys().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setData({ ...data, categories: data });
+      }
+    });
+  };
 
-    useEffect( () => {
-        loadCategory();
-    }, [])
+  //form submit
+  const searchSubmit = () => {};
 
-    return (
-        <div>
-           <div>
-               {}
-           </div>
+  //input on chnage
+  const handelChnage = () => {};
+
+  //search bar
+  const searchForm = () => {
+   return <form onSubmit={searchSubmit}>
+      <span className="input-group-text">
+        <div className="input-group input-group-lg">
+          
+          <div className="input-group-prepend">
+                <select className="btn mr-2" onChange={handelChnage('category')}>
+                    <option value="All">Pick Category</option>
+                    {categories.map((c, i)=> (<option key={i} value={c._id}>{c.name}</option>) )}
+                </select>
+          </div>
+
+          <input
+            type="search"
+            className="form-control"
+            onChange={handelChnage("search")}
+            placeholder="Search Item"
+          />
         </div>
-    )
-}
+
+        <div className="btn input-group-append" style={{border: 'none'}}>
+            <button className="input-group-text">Search</button>
+        </div>
+      </span>
+    </form>;
+  };
+
+  useEffect(() => {
+    loadCategory();
+  }, []);
+
+  return (
+    <div>
+      <div>{searchForm()}</div>
+    </div>
+  );
+};
 
 export default Search;
