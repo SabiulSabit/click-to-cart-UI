e3918cdd8cfc4a0deda86a06c5c360ec29a13fe9
SabitTheBot <iamsabit99@gmail.com>
2021-06-16 18:06:16 +0600

    update styling for product

--- src/core/Card.js
+++ src/core/Card.js
@@ -2,17 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Col } from "react-bootstrap";
 import ShowImage from "./ShowImage";
+import moment from "moment";
 
 const Card = ({ product, viewProductButton = true }) => {
   return (
     <Col md={4} className="mb-3">
       <div className="card">
-        <div className="card-header">{product.name}</div>
+        <div className="card-header name">{product.name}</div>
         <div className="card-body">
+        { product.quantity > 0 ? <span className="badge badge-primary badge-pill float-right">In Stock</span> : <span className="badge badge-danger badge-pill">Out of Stock</span> }
+          
+          <br />
           <ShowImage item={product} url="product" />
-          <p> {product.description.substring(0, 50)} </p>
-          <p> $ {product.price} </p>
+          <p className="lead mt-2"> {product.description.substring(0, 50)} </p>
+          <p className="black-10"> $ {product.price} </p>
+          <p className="black-9">
+            Category: {product.category && product.category.name}{" "}
+          </p>
+          <p className="black-8">
+            Added on: {moment(product.createdAt).fromNow()}{" "}
+          </p>
 
+         
           <Link to={`/product/${product._id}`}>
             {viewProductButton && (
               <button className="btn btn-outline-primary mt-2 mb-2 mr-2">
@@ -20,6 +31,7 @@ const Card = ({ product, viewProductButton = true }) => {
               </button>
             )}
           </Link>
+
           <button className="btn btn-outline-warning mt-2 mb-2  ">
             Add to Cart
           </button>
