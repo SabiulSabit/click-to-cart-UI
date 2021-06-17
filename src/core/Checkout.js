52fb7a592cd47630b5b50727c22a37754c69094b
SabitTheBot <iamsabit99@gmail.com>
2021-06-18 05:00:01 +0600

    checkout button show done

--- src/core/Checkout.js
+++ src/core/Checkout.js
@@ -1,16 +1,37 @@
 import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { Link } from "react-router-dom";
 import { getProducts } from "./apiCore";
 import { Container, Row, Col } from "react-bootstrap";
 import Card from "./Card";
+import { isAuthenticate } from "../auth/index";
 
- const Checkout = ({products}) => {
+const Checkout = ({ products, setRun = (f) => f, run = undefined }) => {
+  //get total price
+  const getTotal = () => {
+    return products.reduce((currentvalue, nextValue) => {
+      return currentvalue + nextValue.count * nextValue.price;
+    }, 0);
+  };
 
-    return (
-        <div>
-            {JSON.stringify(products)}
-        </div>
-    )
-}
+  //show the checkout/singin button
+  const showCheckout = () => {
+    return isAuthenticate() ? (
+      <button className="btn btn-success">Checkout</button>
+    ) : (
+      <Link to="/signin">
+        <button className="btn btn-primary">Sing in to Checkout</button>
+      </Link>
+    );
+  };
+
+  return (
+    <div>
+      <h2>Total: ${getTotal()}</h2>
+
+      {showCheckout()}
+    </div>
+  );
+};
 
 export default Checkout;
