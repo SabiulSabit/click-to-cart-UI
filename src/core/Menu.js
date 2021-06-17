2f827cff3578f98631af6beb10fefb3c8a074765
SabitTheBot <iamsabit99@gmail.com>
2021-06-17 16:01:13 +0600

    cart total prodcut show done

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import { signout, isAuthenticate } from "../auth/index";
+import  {itemTotal} from './cartHelpers'
 
 const isActive = (history, path) => {
   if (history.location.pathname === path) {
@@ -22,6 +23,11 @@ const Menu = ({ history }) => {
         <li className="nav-item">
           <Link className="nav-link" style={isActive(history, "/shop")} to="/shop">
             Shop
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" style={isActive(history, "/cart")} to="/cart">
+            Cart <sup> <small className="cart-badge">{itemTotal()}</small> </sup>
           </Link>
         </li>
 
