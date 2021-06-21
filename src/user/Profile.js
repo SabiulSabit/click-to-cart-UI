28f6dce527459cd56b7ce018eb6cd32aba076101
SabitTheBot <iamsabit99@gmail.com>
2021-06-21 16:53:19 +0600

    basic code added for Profile component

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Layout from "../core/Layout";
 import { Container, Row, Col } from "react-bootstrap";
 import { isAuthenticate } from "../auth/index";
+import {getUserInfo, upadetUserInfo, updateLocalStorageUser} from './apiUser'
 import { Link } from "react-router-dom";
 
-const Profile = () => {
+const Profile = (props) => {
    
-    
+  const [values, setValues] = useState({
+      name: '',
+      password: '',
+      error: '',
+      success: false,
+  })
+
+  const  {name, password, error, success} = values;
+
+  const init = (userId) => {
+   console.log(userId);
+  }
+
+  useEffect( () =>{
+      init(props.match.params.userId);
+  },[] )
 
     return (
-        <div>
-            <h2>Profile</h2>
-        </div>
+        <Layout title="Update Profile" description="E-Commerce Website">
+              <h2 className="mb-4">Profile Update</h2>
+    </Layout>
     )
 }
 
