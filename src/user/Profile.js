7181e8a3b34da82aece0d72848ac7990b6df8687
SabitTheBot <iamsabit99@gmail.com>
2021-06-21 17:18:59 +0600

    user info update done

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,43 +1,100 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { Container, Row, Col } from "react-bootstrap";
 import { isAuthenticate } from "../auth/index";
-import {getUserInfo, upadetUserInfo, updateLocalStorageUser} from './apiUser'
-import { Link } from "react-router-dom";
+import { getUserInfo, upadetUserInfo, updateLocalStorageUser } from "./apiUser";
+import { Link, Redirect } from "react-router-dom";
 
 const Profile = (props) => {
-   
   const [values, setValues] = useState({
-      name: '',
-      password: '',
-      error: false,
-      success: false,
-  })
+    name: "",
+    password: "",
+    error: false,
+    success: false,
+  });
 
-  const  {name, password, error, success} = values;
-  const {token} = isAuthenticate();
+  const { name, password, error, success } = values;
+  const { token } = isAuthenticate();
 
   const init = (userId) => {
-    getUserInfo(userId,token).then(data => {
-        if(data.error ){
-            setValues({...values, error: true});
-        }
-        else{
-            setValues({...values, name: data.name, success: true})
-        }
-    } )
+    getUserInfo(userId, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({ ...values, name: data.name });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init(props.match.params.userId);
+  }, []);
+
+  const handelChnage = name => e => {
+  setValues({...values, error: false, [name]: e.target.value})
+  };
+
+  const clickSubmit = (e) => {
+      e.preventDefault();
+      upadetUserInfo(props.match.params.userId, token,  {name, password} ).then(data =>{
+          if(data.error){
+            setValues({ ...values, error: true });
+          }else{
+              updateLocalStorageUser(data, ()=>{
+                setValues({ ...values, name: data.name, password: data.password, success: true });
+              })
+          }
+      } )
+  };
+
+  const redirectUser = (success) => {
+      if(success) {
+          return <Redirect to="/user/dashboard" />
+      }
   }
 
-  useEffect( () =>{
-      init(props.match.params.userId);
-  },[] )
-
+  const profileUpdate = (name, password) => {
     return (
-        <Layout title="Update Profile" description="E-Commerce Website">
-              <h2 className="mb-4">Profile Update</h2>
-              {JSON.stringify(values)}
+      <Container>
+        <Row>
+          <Col md={12}>
+            <form>
+              <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input
+                  type="text"
+                  onChange={handelChnage("name")}
+                  className="form-control"
+                  value={name}
+                />
+              </div>
+              <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input
+                  type="text"
+                  onChange={handelChnage("password")}
+                  className="form-control"
+                  value={password}
+                />
+              </div>
+
+              <button onClick={clickSubmit} className="btn btn-primary">
+                Submit
+              </button>
+            </form>
+          </Col>
+        </Row>
+      </Container>
+    );
+  };
+
+  return (
+    <Layout title="Update Profile" description="E-Commerce Website">
+      <h2 className="mb-4">Profile Update</h2>
+      {profileUpdate(name, password)}
+      {redirectUser(success)}
     </Layout>
-    )
-}
+  );
+};
 
 export default Profile;
