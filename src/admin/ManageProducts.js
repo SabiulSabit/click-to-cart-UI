2ec65555ae814f855c9876856a1163aabee784bf
SabitTheBot <iamsabit99@gmail.com>
2021-06-21 18:54:27 +0600

    all product show and delete done

--- src/admin/ManageProducts.js
+++ src/admin/ManageProducts.js
@@ -1,36 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import  {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import Layout from "../core/Layout";
 import { Container, Row, Col } from "react-bootstrap";
 import { isAuthenticate } from "../auth/index";
-
-
+import { getProducts, deleteProduct } from "./apiAdmin";
 
 const ManageProducts = () => {
-    return (
-        <Layout
-          title="Manage Products"
-          description="E-Commerce Website"
-          className="container-fluid"
-        >
- 
-    
-          <Container>
-          
-            <Row>
-              {/* {productsBySell.map((product, i) => (
-                <Col md={4} className="mb-3">
-                  <Card key={i} product={product} /> 
-                </Col>
-              ))} */}
-            </Row>
-          </Container>
-    
-          <hr />
-         
-        </Layout>
-      );
-}
+  const [products, setProducts] = useState([]);
+  const { user, token } = isAuthenticate();
+
+  const loadProduct = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  const removeProduct = (prodcutId) => {
+    deleteProduct(prodcutId, user._id, token).then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        loadProduct();
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  return (
+    <Layout
+      title="Manage Products"
+      description="E-Commerce Website"
+      className="container-fluid"
+    >
+      <Container>
+        <Row>
+          <Col md={12} className="mb-3">
+            <h2 className="text-center">Total Products: {products.length}</h2>
+            <hr />
+            <ul className="list-group">
+              {products.map((p, i) => (
+                <li
+                  key={i}
+                  className="list-group-item d-flex justify-content-between align-items-center"
+                >
+                  <strong>{p.name}</strong>
+                  <Link to={`/admin/product/update/${p._id}`}>
+                    <span className="badge badge-warning badge-pill">
+                      Update
+                    </span>
+                  </Link>
+                  <span onClick={ () => removeProduct(p._id) } className="badge badge-danger badge-pill">Delete</span>
+                </li>
+              ))}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+
+      <hr />
+    </Layout>
+  );
+};
 
 export default ManageProducts;
--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -113,7 +113,7 @@ export const updateOrderStatus = (userId, token, orderId, status) => {
 
 //get all products
 export const getProducts = () => {
-  return fetch(`${API}/products`, {
+  return fetch(`${API}/products?limit=100`, {
     method: "GET",
   })
     .then((data) => {
