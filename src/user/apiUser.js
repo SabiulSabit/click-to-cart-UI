a0d5b11e938f5a3cc820eab5fb53fe67bff0205c
SabitTheBot <iamsabit99@gmail.com>
2021-06-21 16:31:24 +0600

    add updateUserInfo method

--- src/user/apiUser.js
+++ src/user/apiUser.js
@@ -19,3 +19,23 @@ export const getUserInfo = (userId, token) => {
       });
   };
   
+
+  //update user info
+  export const upadetUserInfo = (userId, token, userData) =>{
+
+    return fetch(`${API}/user/${userId}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+      body: JSON.stringify(userData)
+    })
+      .then((data) => {
+        return data.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
