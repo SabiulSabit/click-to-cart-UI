726347555d5554867f017b64629ec7742d891ea5
SabitTheBot <iamsabit99@gmail.com>
2021-06-23 19:07:28 +0600

    add footer.js

--- src/core/Footer/Footer.js
+++ src/core/Footer/Footer.js
@@ -1,42 +1,22 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {  faHome, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
+import './Footer.css'
+                                  
 
 const Footer = () => {
   return (
     <div>
-      <footer className="text-center text-lg-start bg-light text-muted">
-        <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
-          <div class="me-5 d-none d-lg-block">
-            <span>Get connected with us on social networks:</span>
-          </div>
+      <footer className="text-lg-start text-muted footer">
+        
+        <h3 className="text-left">Encodemy</h3>
 
-          <div>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-google"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-instagram"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="" class="me-4 text-reset">
-              <i class="fab fa-github"></i>
-            </a>
-          </div>
-        </section>
-
-        <section class="">
-          <div class="container text-center text-md-start mt-5">
-            <div class="row mt-3">
-              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">
-                  <i class="fas fa-gem me-3"></i>Company name
+        <section className="">
+          <div className="container text-center text-md-start mt-5">
+            <div className="row mt-3">
+              <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">
+                  <i className="fas fa-gem me-3"></i>Company name
                 </h6>
                 <p>
                   Here you can use rows and columns to organize your footer
@@ -45,68 +25,68 @@ const Footer = () => {
                 </p>
               </div>
 
-              <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">Products</h6>
+              <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Products</h6>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Angular
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     React
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Vue
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Laravel
                   </a>
                 </p>
               </div>
 
-              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
+              <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Pricing
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Settings
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Orders
                   </a>
                 </p>
                 <p>
-                  <a href="#!" class="text-reset">
+                  <a href="#!" className="text-reset">
                     Help
                   </a>
                 </p>
               </div>
 
-              <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+              <div className="col-md-4 col-lg-4 col-xl-4 mx-auto mb-md-0 mb-4">
+                <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
                 <p>
-                  <i class="fas fa-home me-3"></i> New York, NY 10012, US
+                <FontAwesomeIcon icon={faHome} /> New York, NY 10012, US
                 </p>
                 <p>
-                  <i class="fas fa-envelope me-3"></i>
-                  info@example.com
+                <FontAwesomeIcon icon={faEnvelope} />
+                   info@example.com
                 </p>
                 <p>
-                  <i class="fas fa-phone me-3"></i> + 01 234 567 88
+                <FontAwesomeIcon icon={faPhone} /> + 01 234 567 88
                 </p>
                 <p>
-                  <i class="fas fa-print me-3"></i> + 01 234 567 89
+                <FontAwesomeIcon icon={faPhone} /> + 01 234 567 89
                 </p>
               </div>
             </div>
@@ -114,11 +94,11 @@ const Footer = () => {
         </section>
 
         <div
-          class="text-center p-4"
+          className="text-center p-4"
          
         >
           © 2021 Copyright:
-          <a class="text-reset fw-bold" href="https://mdbootstrap.com/">
+          <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
             MDBootstrap.com
           </a>
         </div>
